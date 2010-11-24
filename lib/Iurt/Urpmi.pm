d362cfeb3376c170ed73d7517db4d2e5e0a128cb
blino <blino@5b7c3c08-40e5-403b-9995-ace06908e4af>
2010-11-24 00:14:49 +0000

    don't resolve hostname in http repository address, using IP address would break vhosts, and we copy resolv.conf in the chroot anyway
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/iurt/trunk@130 5b7c3c08-40e5-403b-9995-ace06908e4af

--- lib/Iurt/Urpmi.pm
+++ lib/Iurt/Urpmi.pm
@@ -24,15 +24,15 @@ sub new {
     if ($run->{use_system_distrib}) {
 	$config->{basesystem_media_root} ||= $run->{use_system_distrib};
     } elsif ($run->{chrooted_urpmi}) {
-	my ($host) = $run->{chrooted_urpmi}{rooted_media} =~ m,(?:file|http|ftp)://([^/]*),;
-	my ($_name, $_aliases, $_addrtype, $_length, @addrs) = gethostbyname($host);
-
-	my $ip = join('.', unpack('C4', $addrs[0]));
-
-	$ip =~ /\d+\.\d+\.\d+\.\d+/
-		or die "FATAL: could not resolve $host ip address";
-
-	$run->{chrooted_urpmi}{rooted_media} =~ s/$host/$ip/;
+	#my ($host) = $run->{chrooted_urpmi}{rooted_media} =~ m,(?:file|http|ftp)://([^/]*),;
+	#my ($_name, $_aliases, $_addrtype, $_length, @addrs) = gethostbyname($host);
+        #
+	#my $ip = join('.', unpack('C4', $addrs[0]));
+        #
+	#$ip =~ /\d+\.\d+\.\d+\.\d+/
+	#	or die "FATAL: could not resolve $host ip address";
+        #
+	#$run->{chrooted_urpmi}{rooted_media} =~ s/$host/$ip/;
 	$run->{chrooted_media} = $run->{chrooted_urpmi}{rooted_media} .
 			"/$run->{distro}/$run->{my_arch}";
 
