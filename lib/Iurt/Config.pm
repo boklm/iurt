38277c523b3fb020927fe346ebcc252daba50136
tv <tv@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-03-24 16:00:11 +0000

    fix not using File::lockf
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/iurt/trunk@3771 5b7c3c08-40e5-403b-9995-ace06908e4af

--- lib/Iurt/Config.pm
+++ lib/Iurt/Config.pm
@@ -7,6 +7,7 @@ use MDK::Common;
 use Iurt::Util qw(plog);
 use strict;
 use Sys::Hostname;
+use File::lockf;
 
 our @EXPORT = qw(
     config_usage
@@ -214,7 +215,6 @@ sub dump_cache_par {
     if (!$status) {
 	unlink $filename;
 	link "$filename.tmp.$daydate", $filename;
-	# FIXME: File::lockf isn't use(d) or require(d) and isn't even in the distro
 	File::lockf::ulock($file);
     } else {
 	plog('WARN', "WARNING: locking the cache file $filename failed (status $status $!), try to lock manually");
