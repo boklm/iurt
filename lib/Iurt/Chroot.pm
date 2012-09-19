7711b2b97e2d8ddf48aa9a9689d46852c270d5b1
pterjan <pterjan@5b7c3c08-40e5-403b-9995-ace06908e4af>
2012-09-19 21:02:35 +0000

    Mount /dev/shm as tmpfs
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/iurt/trunk@5959 5b7c3c08-40e5-403b-9995-ace06908e4af

--- lib/Iurt/Chroot.pm
+++ lib/Iurt/Chroot.pm
@@ -88,7 +88,7 @@ sub clean_and_build_chroot {
 	sudo($config, "--umount", "$chroot/proc");
 	return;
     }
-    if (!sudo($config, '--bindmount', "/dev/shm", "$chroot/dev/shm")) {
+    if (!sudo($config, '--tmpfs', "$chroot/dev/shm")) {
 	plog('WARNING', "Failed to mount /dev/shm");
     }
     if ($run->{icecream}) {
